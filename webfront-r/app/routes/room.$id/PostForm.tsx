4d1af15f9e22503d0dc389955e76a721dcafa069
kooooohe <1211lta+github@gmail.com>
2025-05-09 00:02:37 +0900

    updated

--- webfront-r/app/routes/room.$id/PostForm.tsx
+++ webfront-r/app/routes/room.$id/PostForm.tsx
@@ -204,15 +204,6 @@ const PostForm: React.FC<PostFormProps> = ({ onSubmit, postCount }) => {
               <button
                 type="button"
                 className="p-2 rounded hover:bg-gray-700 transition text-cyan-300"
-                onClick={() => fileInputRef.current?.click()}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
-              </button>
-              <button
-                type="button"
-                className="p-2 rounded hover:bg-gray-700 transition text-cyan-300"
                 onClick={insertAA}
               >
                 <span className="text-sm font-mono">AA</span>
